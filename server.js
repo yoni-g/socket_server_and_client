0d06076f0c2fc77d82de331445bc2d1e2bcc9e87
Yonathan Goriachnick <yonig770@gmail.com>
2021-06-07 09:44:12 +0300

    added client-server logic

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-const { Socket } = require("socket.io");
 
 const port = process.env.PORT || 8080;
 
@@ -14,40 +10,32 @@ const http = require("http").createServer((req, response) => {
     response.write(html)
     response.end();  
 });
-
 const io = require("socket.io")(http);
-// console.log(io);
 
 io.on("connection", (socket) => {
 
   console.info("new client is connected!")
-  socket.emit("welcome", "welcome to the socket")
+  socket.emit("welcome", "Welcome to the socket")
 
-  setInterval(() => {
-    socket.emit("time", new Date().toTimeString())
-  }, 1000)
-
-  socket.on("message", (message) => {
+  socket.on("join-chat-request", (message) => {
     console.log("message from cleint >>");
     console.info(message)
+    handleJoinChatRequest(socket)
+
   });
 
   socket.on("disconnect", (reason) => {
-
     console.log("client disconnected with reason >>");
     console.info(reason)
   });
 
 });
 
-io.on("message", (data) => {
-  console.log(data);
-});
-
-// handle the event sent with socket.emit()
-io.on("salutations", (elem1, elem2, elem3) => {
-  console.log(elem1, elem2, elem3);
-});
+const handleJoinChatRequest = (socket) => {
+  setTimeout(() => {
+    socket.emit("joined-chat", "Welcome to the Chat")
+  }, 2000);
+}
 
 http.listen(port, () => {
   console.log("server is listening on " + port)
